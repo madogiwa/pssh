24c698462da39491b3c52058f02cf4d8f3f695e4
Andy Cobaugh <andrewcobaugh@gmail.com>
2017-05-15 20:51:48 -0400

    Apply the trailing comment patch from https://github.com/lilydjwg/pssh/issues/69

--- psshlib/psshutil.py
+++ psshlib/psshutil.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2003-2008, Brent N. Chun
 
 import fcntl
+import re
 import string
 import sys
 
@@ -34,7 +35,8 @@ def read_host_file(path, default_user=None, default_port=None):
 
     hosts = []
     for line in lines:
-        # Skip blank lines or lines starting with #
+        # Skip blank lines, trailing comments, or lines starting with #
+        line = re.sub('#.*', '', line)
         line = line.strip()
         if not line or line.startswith('#'):
             continue
